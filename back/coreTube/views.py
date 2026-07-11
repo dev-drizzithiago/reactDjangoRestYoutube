1d8003f340847ee9c3e8bcb0c3345286ced781a4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-10 22:18:11 -0300

    Update views.py

--- back/coreTube/views.py
+++ back/coreTube/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from serializers import *
+
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializers_class = UserSerializer
