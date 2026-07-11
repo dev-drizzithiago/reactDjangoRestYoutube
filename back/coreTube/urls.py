48ad20398fb627c562b436a29a86f577456e3a4a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-10 22:18:08 -0300

    Update urls.py

--- back/coreTube/urls.py
+++ back/coreTube/urls.py
@@ -1,18 +1,8 @@
 
 from django.urls import path, include
-from django.contrib.auth.models import User
 
 from rest_framework import routers, serializers, viewsets
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializers_class = UserSerializer
-
+from views import *
 
 router = routers.DefaultRouter()
 router.register(f'users', UserViewSet)
