15273c2cb946dd6f99be80ef6fe7ba4c86a6d173
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-16 14:41:28 -0300

    Update main.jsx

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ProviederLoginContext } from './Components/Contexts/ContextLogin.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ProviederLoginContext>      
+        <App />
+      </ProviederLoginContext>
+    </BrowserRouter>
   </StrictMode>,
 )
