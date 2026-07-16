5ede9cdeabbc64b44b7d194f4b9fc1e59fd69efb
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-16 14:41:25 -0300

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,22 @@
 import './App.css'
 import JanelaLogin from './Components/JanelaLogin/JanelaLogin';
 import { useLoginContext } from './Components/Contexts/ContextLogin';
+import IndexBar from './Rotas/IndexBar';
 
 function App() {
   const { tokenLogin, setTokenLogin } = useLoginContext()
 
   return (
     <div className='app_principal'>
+      <div>
+        <h3>Titulo</h3>
+      </div>
 
-      <div><h3>Titulo</h3></div>
-
-      {!tokenLogin && <JanelaLogin />}
-
-
+      <div className='app_divIndexBar'>
+        <IndexBar />
+      </div>
     </div>
   )
-    
 }
 
 export default App
