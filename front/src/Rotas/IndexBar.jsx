a321d032086420e2a827a6dff7f368fbec7f1e73
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-16 14:41:31 -0300

    Create IndexBar.jsx

---
+++ front/src/Rotas/IndexBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const IndexBar = () => {
+
+  return (
+    <div>
+         <ul className='nav-listaLinks'>
+            <li >
+                <NavLink to='login'>                    
+                    Login
+                </NavLink> 
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default IndexBar
