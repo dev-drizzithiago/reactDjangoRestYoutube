3866f085d9d251ad42f06f934f446093361e83b1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-16 14:41:34 -0300

    Update RotaPrincipal.jsx

--- front/src/Rotas/RotaPrincipal.jsx
+++ front/src/Rotas/RotaPrincipal.jsx
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
+import JanelaLogin from "../Components/JanelaLogin/JanelaLogin";
 
 <Route>
-
-
+    <Route to='login' element={<JanelaLogin />}/>
 </Route>
