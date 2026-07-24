af8fc35df0e098b56004c46946c38fbf5e1b35f3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-24 14:24:46 -0300

    Update JanelaMp3.jsx

--- front/src/Components/JanelaMp3/JanelaMp3.jsx
+++ front/src/Components/JanelaMp3/JanelaMp3.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function JanelaMp3() {
+
+  const navigate = useNavigate()
+
   return (
     <div>
         <div>
             <h3>MP3 Salvos</h3>
+        </div>
+
+        <div>
+          <button onClick={() => navigate('/')}>Voltar</button>
         </div>
     </div>
   )
