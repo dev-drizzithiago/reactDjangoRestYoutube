67c9bf5c5893e57ba81c0dcb4ac73cef10038c17
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-07-24 14:07:02 -0300

    Update JanelaLinks.jsx

--- front/src/Components/JanelaLinks/JanelaLinks.jsx
+++ front/src/Components/JanelaLinks/JanelaLinks.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function JanelaLinks() {
+
+  const navigate = useNavigate()
   return (
     <div>
         <div>
             <h3>Links Salvos</h3>
+        </div>
+
+        <div>
+          <button onClick={() => navigate('/')}>Voltar</button>
         </div>
     </div>
   )
