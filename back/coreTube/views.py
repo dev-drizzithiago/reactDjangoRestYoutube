eb78e38f3088823a2fccea84507f91aeb8634286
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-08-01 14:20:54 -0300

    Update views.py

--- back/coreTube/views.py
+++ back/coreTube/views.py
@@ -1,6 +1,10 @@
+from django.contrib.admin import actions
 from django.shortcuts import render
 
+from rest_framework.decorators import action
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from django.views.generic import detail
 from rest_framework import viewsets
 from .serializers import *
 
@@ -9,3 +13,19 @@ from .serializers import *
 class DadosYoutubeViewSet(viewsets.ModelViewSet):
     queryset = DadosYoutube.objects.all()
     serializer_class = DadosYoutubeSerializer
+
+    @action(detail=False, methods=['POST'])
+    def link(self, request):
+        msg_processo = None
+        err_processo = None
+        ret_processo = None
+
+        link_front = request.data
+        print(link_front)
+
+        return Response({
+            "msg_processo": msg_processo,
+            "err_processo": err_processo,
+            "ret_processo": ret_processo,
+        }, status=err_processo)
+
